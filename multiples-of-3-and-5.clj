20527983dbd75efa9c7d6258b876a1ac8b3e8d4c
Tyler Funk <tfunk@fastmail.fm>
2014-05-05 15:44:48 -0500

    Completed Problem 1

---
+++ multiples-of-3-and-5.clj
@@ -0,0 +1,19 @@
+;; Project Euler
+;;
+;; Multiples of 3 and 5
+
+(ns com.teamgamestar
+  (:require clojure.repl
+            clojure.string))
+
+(defn is-divisible
+  [n]
+  (or (= 0 (mod n 3))
+      (= 0 (mod n 5))))
+
+(reduce + (reduce (fn [nums n]
+          (if (is-divisible n)
+            (conj nums n)
+            nums))
+        []
+        (take 1000 (iterate inc 0))))
